904fd2fc499da71052e207a04dad1ca3a3faaa92
Yannick <yannick.boogaerts@technifutur.be>
2022-01-10 09:15:27 +0100

    refactorisation des tests

--- test/be/technifutur/java2021/MyFramorkTest.java
+++ test/be/technifutur/java2021/MyFramorkTest.java
@@ -6,17 +6,30 @@ import org.junit.jupiter.api.Test;
 public class MyFramorkTest {
     @Test
     void testStartHelloWorld(){
+        Application app = getHelloWorld();
+        new MyFramework().start(app);
+        EasyMock.verify(app);
+    }
+
+    private Application getHelloWorld() {
         Application app = EasyMock.createMock(Application.class);
         app.start();
         EasyMock.expect(app.isFinsish()).andReturn(true);
         EasyMock.expect(app.getScreen()).andReturn("Hello world");
         EasyMock.replay(app);
-        new MyFramework().start(app);
-        EasyMock.verify(app);
+        return app;
     }
 
     @Test
     void testStartHelloYannick(){
+        Application app = getHelloYannick();
+        MyFramework myFramework = new MyFramework();
+        myFramework.setSupplier(()->"Yannick");
+        myFramework.start(app);
+        EasyMock.verify(app);
+    }
+
+    private Application getHelloYannick() {
         Application app = EasyMock.createMock(Application.class);
         app.start();
         EasyMock.expect(app.isFinsish()).andReturn(false);
@@ -25,10 +38,7 @@ public class MyFramorkTest {
         EasyMock.expect(app.isFinsish()).andReturn(true);
         EasyMock.expect(app.getScreen()).andReturn("Hello Yannick");
         EasyMock.replay(app);
-        MyFramework myFramework = new MyFramework();
-        myFramework.setSupplier(()->"Yannick");
-        myFramework.start(app);
-        EasyMock.verify(app);
+        return app;
     }
 
 }
